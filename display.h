551b01ae8b3c275cabd7478ec7fa847f3d58bf4d
sagarthereal <46030414+sagarthereal@users.noreply.github.com>
2024-08-07 17:10:29 +0530

    Update display.h

--- display.h
+++ display.h
@@ -9,7 +9,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <linux/kd.h>
@@ -18,6 +18,7 @@
 #include <signal.h>
 #include <sys/time.h>
 #include <cairo.h>
+using namespace std;
 
 class display_t
 {
@@ -36,7 +37,7 @@ class display_t
 		static uint32_t current_buffer_id_in_use;
 		uint32_t width;
 		uint32_t height;
-		uint16_t bpp;
+		uint16_t bpp = 16;
 		struct drm_mode_create_dumb create_dumb = {0};
 		static cairo_t *cr1;
 		static cairo_t *cr2;
